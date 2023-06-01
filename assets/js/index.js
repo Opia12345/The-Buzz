699a9b17c26d9e59a798803ae1287813f3827dac
Opia12345 <119626942+Opia12345@users.noreply.github.com>
2023-06-01 04:56:45 -0700

    New API addition

--- assets/js/index.js
+++ assets/js/index.js
@@ -173,3 +173,243 @@ function validateFormSignin(){
 }
 
 //Form Validation Ends
+
+// Public API Build
+
+const generalBtn = document.getElementById('general');
+const techBtn = document.getElementById('tech');
+const healthBtn = document.getElementById('health');
+const businessBtn = document.getElementById('business');
+const entertainmentBtn = document.getElementById('entertainment');
+const scienceBtn = document.getElementById('science');
+const sportsBtn = document.getElementById('sports');
+const searchBtn = document.getElementById('searchBtn');
+
+const newsQuery = document.getElementById('newsQuery');
+const newsType = document.getElementById('newsType');
+const newsDetails = document.getElementById('newsDetails');
+
+//Array
+var newsDataArr = [];
+
+
+//APIs
+
+var API_KEY = '6e95606cc92539c6a2aff4a3377265af';
+var GENERAL_NEWS = 'https://gnews.io/api/v4/top-headlines?category=genera&country=us&language=en&apikey=';
+var SPORT_NEWS = 'https://gnews.io/api/v4/top-headlines?category=sports&country=us&language=en&apikey=';
+var BUSINESS_NEWS ='https://gnews.io/api/v4/top-headlines?category=business&country=us&language=en&apikey=';
+var TECH_NEWS ='https://gnews.io/api/v4/top-headlines?category=technology&country=us&language=en&apikey=';
+var HEALTH_NEWS ='https://gnews.io/api/v4/top-headlines?category=health&country=us&language=en&apikey=';
+var ENTERTAINMENT_NEWS ='https://gnews.io/api/v4/top-headlines?category=entertainment&country=us&language=en&apikey=';
+var SCIENCE_NEWS ='https://gnews.io/api/v4/top-headlines?category=science&country=us&language=en&apikey=';
+var SEARCH_NEWS ='https://gnews.io/api/v4/search?q&apikey=';
+
+
+// const myArray = [
+//     fetch(`https://newsdata.io/api/1/news?apikey=pub_23789d40612bb2134504db9647d263fa88499&q=Science`), // ordersAf
+//     fetch(`https://newsdata.io/api/1/news?apikey=pub_23789d40612bb2134504db9647d263fa88499&q=Entertainment`),
+//     fetch(`https://newsdata.io/api/1/news?apikey=pub_23789d40612bb2134504db9647d263fa88499&q=Business`),
+//     fetch(`https://newsdata.io/api/1/news?apikey=pub_23789d40612bb2134504db9647d263fa88499&q=Tech`),
+//     fetch(`https://newsdata.io/api/1/news?apikey=pub_23789d40612bb2134504db9647d263fa88499&q=Sports`),
+//     fetch(`https://newsdata.io/api/1/news?apikey=pub_23789d40612bb2134504db9647d263fa88499&q=Health`)
+    
+//   ]
+//   async function fetchData(Array){
+
+//   // fails the rest of the fetch if one fails
+//   // then throws an error
+//     try{
+//       const res = await Promise.all(Array) 
+//       const data = await Promise.all(res.map((item) => { 
+//         return data.json();
+//       }))
+
+//     }
+//     catch(e){
+//       console.log("multiple fetch failed");
+//     }
+//     //   multpleData.current[0].data
+//     //   multpleData.current[2].data
+//     //   multpleData.current[3].data
+//     //   multpleData.current[4].data
+//     //   multpleData.current[5].data
+//     //   multpleData.current[1].data
+
+//   }
+//   fetchData(myArray);
+
+
+    window.onload = (function(){
+        newsType.innerHTML = "<h4>Trending</h4>"
+        fetchGeneralNews();
+    })
+
+sportsBtn.addEventListener('click', function(){
+    newsType.innerHTML = '<h4>Sports</h4>';
+    fetchSportNews();
+});
+
+businessBtn.addEventListener('click', function(){
+    newsType.innerHTML = '<h4>Business</h4>';
+    fetchBusinessNews();
+});
+
+healthBtn.addEventListener('click', function(){
+    newsType.innerHTML = '<h4>Health</h4>';
+    fetchHealthNews();
+});
+
+entertainmentBtn.addEventListener('click', function(){
+    newsType.innerHTML = '<h4>Entertainment</h4>';
+    fetchEntertainmentNews();
+});
+
+techBtn.addEventListener('click', function(){
+    newsType.innerHTML = '<h4>Technology</h4>';
+    fetchTechNews();
+});
+
+scienceBtn.addEventListener('click', function(){
+    newsType.innerHTML = '<h4>Science</h4>';
+    fetchScienceNews();
+});
+
+searchBtn.addEventListener('click', function(){
+    newsType.innerHTML = '<h4>Search: '+newsQuery.value+ '</h4>';
+    fetchQueryNews();
+});
+
+
+const fetchGeneralNews = async() =>{
+    const response = await fetch(GENERAL_NEWS + API_KEY);
+    newsDataArr = [];
+    if (response.status >= 200 && response.status < 300) {
+        const myJson = await response.json();
+        console.log(myJson);
+        newsDataArr = myJson.articles;
+    } else {
+        console.log(response.status, response.statusText);
+    }
+    displayNews();
+}
+
+const fetchSportNews = async() =>{
+    const response = await fetch(SPORT_NEWS + API_KEY);
+    newsDataArr = [];
+    if (response.status >= 200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        console.log(response.status, response.statusText);
+    }
+    displayNews();
+}
+
+const fetchBusinessNews = async() =>{
+    const response = await fetch(BUSINESS_NEWS + API_KEY);
+    newsDataArr = [];
+    if (response.status >= 200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        console.log(response.status, response.statusText);
+    }
+    displayNews();
+}
+
+const fetchHealthNews = async() =>{
+    const response = await fetch(HEALTH_NEWS + API_KEY);
+    newsDataArr = [];
+    if (response.status >= 200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        console.log(response.status, response.statusText);
+    }
+    displayNews();
+}
+
+const fetchEntertainmentNews = async() =>{
+    const response = await fetch(ENTERTAINMENT_NEWS + API_KEY);
+    newsDataArr = [];
+    if (response.status >= 200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        console.log(response.status, response.statusText);
+    }
+    displayNews();
+}
+
+const fetchTechNews = async() =>{
+    const response = await fetch(TECH_NEWS + API_KEY);
+    newsDataArr = [];
+    if (response.status >= 200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        console.log(response.status, response.statusText);
+    }
+    displayNews();
+}
+
+const fetchScienceNews = async() =>{
+    const response = await fetch(SCIENCE_NEWS + API_KEY);
+    newsDataArr = [];
+    if (response.status >= 200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        console.log(response.status, response.statusText);
+    }
+    displayNews();
+}
+
+const fetchQueryNews = async() =>{
+    if(newsQuery.value == null){
+        return;
+    }
+    const response = await fetch(SEARCH_NEWS+newsQuery.value+'&apiKey='+ API_KEY);
+    newsDataArr = [];
+    if (response.status >= 200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        console.log(response.status, response.statusText);
+    }
+    displayNews();
+}
+
+
+function displayNews(){
+    
+  const html = newsDataArr.map((news) => {
+return `
+<div class="col-sm-12 col-lg-4 mb-4">
+<a href="${news.url}">
+<div class="stack-body">
+<img class="post-image" src="${news.image}"  
+style="background: rgba(0, 0, 0, 0.459);
+background-size: cover;
+width: 100%;
+border-radius: 5px;
+height: 12em;
+background-blend-mode: darken;" 
+alt="news img">
+</div>
+<div class="txt-1" 
+style="position: relative; margin-top: 1em;">  
+<h4 style= "font-size: 17px; font-weight: bolder;">${news.title}</h4>
+<h6>${news.source.name}</h6>
+<small class="text-muted">${news.publishedAt}</small>
+</div>
+</a>
+</div>
+</div>`;
+}).join('');
+newsContainer.innerHTML = html;
+};
+
+
+
